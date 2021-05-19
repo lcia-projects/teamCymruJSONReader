dbd3c2a62e7b7a351c28034152fac980bd6b55e2
darrellmiller <darrellrhodesmiller@gmail.com>
2021-05-19 15:18:42 -0500

    basics work, data file currently hard coded, multi-threaded, drastic speed improvement

--- libGeoIP.py
+++ libGeoIP.py
@@ -3,13 +3,13 @@ import time
 
 class getGeoIP:
     def __init__(self):
-        test=1
+        self.reader = geoip2.database.Reader('./geoip/GeoLite2-City.mmdb')
 
     def getGeoIPCity(self,ip):
-        reader = geoip2.database.Reader('./geoip/GeoLite2-City.mmdb')
+
         responseData={}
         try:
-            response = reader.city(ip)
+            response = self.reader.city(ip)
             responseData['country_code'] = response.country.iso_code
             responseData['most_specific.name'] = response.subdivisions.most_specific.name
             responseData['most_specific.iso_code'] = response.subdivisions.most_specific.iso_code
@@ -19,9 +19,7 @@ class getGeoIP:
             responseData['location']['lat'] = response.location.latitude
             responseData['location']['lon'] = response.location.longitude
             responseData['country.name'] = response.country.name
-            reader.close()
             return responseData.copy()
         except:
             responseData=None
-            reader.close()
             return responseData
